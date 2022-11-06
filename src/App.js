da1fcf2bfb26969a6419e83b1483d8228ce7deb4
hardik-143 <dhardik1430@gmail.com>
2022-11-06 11:11:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import SpaceMission from "./graphql";
+import {
+  MDBCard,
+  MDBCardText,
+  MDBContainer,
+  MDBRow,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardLink,
+} from "mdb-react-ui-kit";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const loadSpaceMission = async () => {
+    const spaceMissions = await SpaceMission.getSpaceMission(10);
+    setData(spaceMissions);
+  }
+
+  useEffect(()=> {
+    loadSpaceMission();
+  },[])
+  console.log("data", data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <MDBContainer style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "720px",
+        alignContent: "center",
+      }}>
+        <MDBRow>
+          <h1>Past Missions' Details</h1>
+            {data.map((item,index)=>(
+            <>
+              <MDBCard key={index}
+                style={{maxWidth:"22rem", maxHeight:"24rem"}}>
+              {/* <MDBCardImage src={item && item.ships[0] && item.ships[0].image ? item.ships[0].image :  }/> */}
+              {/* position="top"
+              alt={item.mission_name} */}
+              <MDBCardBody>
+                <MDBCardTitle>{item.mission_name}</MDBCardTitle>
+                <MDBCardText>{item.launch_site.site_name_long}</MDBCardText>
+                <MDBCardText>{item.rocket.rocket_name}</MDBCardText>
+                <MDBCardText>{item.rocket.rocket.description}</MDBCardText>
+                <MDBCardText>{item.rocket.rocket.success_rate_pct}</MDBCardText>
+                <MDBCardLink>{item.rocket.rocket.wikipedia}</MDBCardLink>
+              </MDBCardBody>
+              </MDBCard>
+            </>
+          ))}
+        </MDBRow>
+      </MDBContainer>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+// import appollo provider from appollo client
+import {ApolloProvider} from '@apollo/client';
+import {apolloClient} from "./graphql"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={apolloClient}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
@@ -15,3 +22,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
